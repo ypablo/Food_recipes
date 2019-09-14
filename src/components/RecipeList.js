5e4ac5a7cc0e28adf1ba5a40a5d910f1c403231f
ypablo <yfedora@gmail.com>
2019-09-14 15:24:31 +0100

    Search component

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -3,10 +3,25 @@ import Recipe from './Recipe';
 
 export default class RecipeList extends Component {
     render() {
+        const { recipes } = this.props;
         return (
-            <div>
-                Hello from recipe list
-                <Recipe />
+            <div className="container py-5">
+                {/*title */}
+                <div className="row">
+                    <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
+                        <h1 className="text-slanted">recipe list</h1>
+                    </div>
+                </div>
+                {/* end of title */}
+                <div className="row">
+                    {recipes.map(recipe => (
+                        <Recipe
+                            key={recipe.recipe_id}
+                            recipe={recipe}>
+                        </Recipe>
+                    ))}
+                </div>
+
             </div>
         )
     }
