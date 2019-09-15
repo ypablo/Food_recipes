9e8f3aee8be1fd8445f929adde58b938eef6de68
ypablo <yfedora@gmail.com>
2019-09-15 13:48:41 +0100

    Add search capability

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,9 +8,10 @@ export default class Search extends Component {
                 <div className="row">
                     <div className="col-10 mx-auto col-md-8 mt-5 text-center">
                         <h1 className="text-slanted text-capitalize">
-                            search recipes with {""} <strong className="text-orange">
+                            search recipes with {""} 
+                            <strong className="text-orange">
                                 Food2Fork
-                        </strong>
+                            </strong>
                         </h1>
                         <form className="mt-4">
                             <label htmlFor="search" className="text-capitalize">
@@ -24,9 +25,11 @@ export default class Search extends Component {
                                     placeholder="onion, carrots, leek"
                                     value={search}
                                     onChange={handleChange} />
-
                                 <div className="input-group-append">
-                                    <button type="submit" className="input-group-text bg-primary text-white" onClick={this.handleSubmit}>
+                                    <button 
+                                    type="submit" 
+                                    className="input-group-text bg-primary text-white" 
+                                    onClick={handleSubmit}>
                                         <i className="fas fa-search" />
                                     </button>
                                 </div>
@@ -34,7 +37,6 @@ export default class Search extends Component {
                         </form>
                     </div>
                 </div>
-
             </div>
         )
     }
--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -5,12 +5,40 @@ import { recipeData } from '../data/tempList';
 
 export default class Recipes extends Component {
     constructor(props) {
-        super(props)
-
+        super(props);
+        this.getRecipes = this.getRecipes.bind(this);
     }
     state = {
         recipes: recipeData,
-        search: ""
+        search: "",
+        url: `https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`,
+        base_url: `https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`,
+        query: '&q=',
+        error: ""
+    }
+
+    async getRecipes(){
+        try{
+            const data = await fetch(this.state.url);
+            const jsonData = await data.json();
+            if(jsonData.recipes.length === 0) {
+                this.setState({
+                    error: "I'm sorry but your search did not return any recipes, please try again."
+                });
+                } else {
+                this.setState({
+                    recipes: jsonData.recipes,
+                    error: ""
+                    })
+                }    
+            }
+        catch(error){
+            console.log(error);
+        }
+    }
+
+    componentDidMount(){
+        this.getRecipes();
     }
 
     handleChange = (e) => {
@@ -20,7 +48,14 @@ export default class Recipes extends Component {
     }
 
     handleSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
+        const { base_url, query, search} = this.state;
+        this.setState({
+            url: `${base_url}${query}${search}`,
+            search: ""
+        }, 
+        () => this.getRecipes()
+        );
     }
 
     render() {
@@ -28,10 +63,20 @@ export default class Recipes extends Component {
             <div>
                 <Search
                     search={this.state.search}
-                    handleChange={this.handleSubmit}
-                    handleChange={this.handleChange} />
-                <RecipeList recipes={this.state.recipes} />
+                    handleChange={this.handleChange} 
+                    handleSubmit={this.handleSubmit}/>
+                    {this.state.error ? (
+                        <section>
+                            <div className="row">
+                                <div className="col">
+                                    <h2 className="text-orange text-center text-uppercase mt-5">
+                                        {this.state.error}
+                                    </h2>
+                                </div>
+                            </div>
+                        </section>):(
+                    <RecipeList recipes={this.state.recipes} />)}
             </div>
-        )
+        );
     }
 }
